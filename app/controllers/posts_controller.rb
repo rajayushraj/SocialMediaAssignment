64970d2e32a6e279bedb4471e2791792b6724766
Ayush Raj <ayush1224raj@gmail.com>
2021-01-30 17:18:54 +0530

    added functionality to create a new post show and index to show all post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,33 @@
 class PostsController < ApplicationController
+  def index
+		@posts=Post.all
+	end
+	def new
+		@post=Post.new
+	end
+	def show
+		@post=Post.find(params[:id])
+	end
+	def create
+		@post = Post.new(post_params) 
+    @post.user = current_user
+    if @post.save 
+      flash[:notice] = "Post was created successfully." 
+      redirect_to @post
+    else 
+      render 'new' 
+    end
+
+	end
+	def destroy
+		@post=Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	private
+
+	def post_params
+    params.require(:post).permit(:description)
+   end
 end

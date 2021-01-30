e573d8ae0cee82ed83716dbc3f0bcf6c51ad3606
Ayush Raj <ayush1224raj@gmail.com>
2021-01-30 16:32:35 +0530

    Added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :fname ,presence: true,length: { minimum: 3,maximum: 25}
+  validates :lname ,presence: true,length: { minimum: 3,maximum: 25}
+  validates :acc_status,presence: true,:inclusion => { :in => 0..2 }
 end

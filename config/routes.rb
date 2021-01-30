c50b437179418dbd4b1a3da3374f1842ac515f51
Ayush Raj <ayush1224raj@gmail.com>
2021-01-30 17:04:06 +0530

    Added routes and all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
+  resources:users,only: [:show] do
+  	resources:friendships,only: [:create,:destroy]
+  end
+  resources:posts do
+  	resources:comments,only: [:new,:create,:show,:index,:destroy]
+  	resources:likes,only: [:create]
+  end
 end

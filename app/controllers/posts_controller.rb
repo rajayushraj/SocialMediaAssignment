1c360da0b1b1406ed28d6750d7f95e122a17aff7
Ayush Raj <ayush1224raj@gmail.com>
2021-02-02 17:44:06 +0530

    Added field status to post table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,6 +39,6 @@ class PostsController < ApplicationController
 	private
 
 	def post_params
-    params.require(:post).permit(:description)
+    params.require(:post).permit(:description,:status)
    end
 end
